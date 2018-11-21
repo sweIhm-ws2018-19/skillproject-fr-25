76ed81b6b37087c1305dae4eff65a645e27817c2
schroedp <43878808+schroedp@users.noreply.github.com>
2018-11-21 14:12:39 +0100

    Delete SpeechStrings.java

--- gehirnjogging/SpeechStrings.java
+++
@@ -1,9 +0,0 @@
-package main.java.gehirnjogging;
-
-public class SpeechStrings {
-	public static final String SKILLNAME = "Gehirnjogging.";
-	public static final String WELCOME = "Hallo <break time=\"1s\"/>. Willkomemen beim Gehirnjogging Skill.";
-	public static final String WELCOME_REPROMPT = "Bist du noch da <break time=\"1s\"/>? Der Gehirnjogging Skill ist bereits offen.";
-	public static final String STOP = "Auf Wiedersehen.";
-
-}
