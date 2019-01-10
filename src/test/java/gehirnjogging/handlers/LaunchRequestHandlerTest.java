506ecbe56ac1f0658281cd29ac9944628295bf59
RichardReik <richard.reik@hm.edu>
2019-01-10 19:55:55 +0100

    TestsFinalVersion

--- src/test/java/gehirnjogging/handlers/LaunchRequestHandlerTest.java
+++ src/test/java/gehirnjogging/handlers/LaunchRequestHandlerTest.java
@@ -2,16 +2,26 @@ package gehirnjogging.handlers;
 
 import com.amazon.ask.dispatcher.request.handler.HandlerInput;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import com.amazon.ask.model.Response;
+import gehirnjogging.Logic;
+import gehirnjogging.TestUtil;
 import org.junit.Test;
 import org.mockito.Mockito;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.when;
 
 
 public class LaunchRequestHandlerTest{
 	LaunchRequestHandler handler = new LaunchRequestHandler();
+    Logic haus = new Logic();
 
     @Test
     public void testCanHandle(){
@@ -23,6 +33,61 @@ public class LaunchRequestHandlerTest{
     
     @Test
     public void testHandle() {
+        haus.newGame();
+        haus.initializeNumbers();
+        haus.initializeQuestions();
+        haus.STATUS_ID = 0;
+        haus.inizialPoints();
+
+
+        Map<String, Object> sessAtt = new HashMap<String, Object>();
+        sessAtt.put("pause", null);
+        final HandlerInput mockInput = TestUtil.mockHandlerInput(null, sessAtt, sessAtt, null);
+
+        Optional<Response> res = handler.handle(mockInput);
+        assertTrue(res.isPresent());
+        Response response = res.get();
+        System.out.println(response.getOutputSpeech().toString());
+        assertTrue(response.getOutputSpeech().toString().contains("Willkommen bei Train your Brain Gehirnjogging!"));
+        assertEquals(0, haus.STATUS_ID);
+    }
+
+    @Test
+    public void testHandlep2() {
+        haus.newGame();
+        haus.initializeNumbers();
+        haus.initializeQuestions();
+        haus.STATUS_ID = 0;
+        haus.inizialPoints();
+
+
+        Map<String, Object> sessAtt = new HashMap<String, Object>();
+        sessAtt.put("pause", "yes");
+        sessAtt.put("player1","2");
+        sessAtt.put("player2","2");
+        sessAtt.put("player3","2");
+        sessAtt.put("currentPlayer","2");
+        sessAtt.put("RICHTIGE_ANTWORT","2");
+
+//        final HandlerInput mockInput = TestUtil.mockHandlerInput(null, sessAtt, sessAtt, null);
+//
+//        Optional<Response> res = handler.handle(mockInput);
+//        assertTrue(res.isPresent());
+//        Response response = res.get();
+//        System.out.println(response.getOutputSpeech().toString());
+//
+//        assertEquals(2,haus.player1);
+//        assertEquals(2,haus.player2);
+//        assertEquals(2,haus.player3);
+//        assertEquals(2,haus.currentPlayer);
+//        assertEquals(2,haus.RICHTIGE_ANTWORT);
+//
+//
+//
+//
+//        assertTrue(response.getOutputSpeech().toString().contains("Willkommen bei Train your Brain Gehirnjogging!"));
+
+
     }
 
 }
--- src/test/java/gehirnjogging/handlers/NoHandlerTest.java
+++ src/test/java/gehirnjogging/handlers/NoHandlerTest.java
@@ -1,4 +1,128 @@
 package gehirnjogging.handlers;
 
+import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+import com.amazon.ask.model.Response;
+import gehirnjogging.Logic;
+import gehirnjogging.TestUtil;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
 public class NoHandlerTest {
+    gehirnjogging.handlers.NoHandler handler = new gehirnjogging.handlers.NoHandler();
+    Logic haus = new Logic();
+
+
+    @Test
+    public void testCanHandle() {
+        final HandlerInput inputMock = Mockito.mock(HandlerInput.class);
+        when(inputMock.matches(any())).thenReturn(true);
+        assertTrue(handler.canHandle(inputMock));
+    }
+
+    @Test
+    public void testHandle() {
+        Logic haus = new Logic();
+
+        Map<String, Object> sessAtt = new HashMap<String, Object>();
+        sessAtt.put("test", "tests");
+        final HandlerInput mockInput = TestUtil.mockHandlerInput(null, sessAtt, sessAtt, null);
+
+        haus.newGame();
+        haus.initializeNumbers();
+        haus.initializeQuestions();
+        haus.STATUS_ID = 0;
+        haus.inizialPoints();
+
+
+        Optional<Response> res = handler.handle(mockInput);
+        assertTrue(res.isPresent());
+        Response response = res.get();
+        assertTrue(response.getOutputSpeech().toString().contains("Schade, dass du doch nicht spielen möchtest, ich hoffe, dass du bald wiederkommst"));
+        assertEquals(1,haus.STATUS_ID);
+
+
+        haus.STATUS_ID = 1;
+        haus.EINSTELLUNGS_COUNTER_R =1;
+        res = handler.handle(mockInput);
+        assertTrue(res.isPresent());
+        response = res.get();
+        assertEquals(5,haus.STATUS_ID);
+        assertEquals(5,haus.EINSTELLUNGS_ID);
+
+        haus.STATUS_ID = 1;
+        haus.EINSTELLUNGS_COUNTER_R =3;
+        res = handler.handle(mockInput);
+        assertTrue(res.isPresent());
+        response = res.get();
+        assertEquals(3,haus.STATUS_ID);
+        assertEquals(5,haus.EINSTELLUNGS_ID);
+
+
+        haus.STATUS_ID = 1;
+        haus.random = 1;
+        haus.counter =0;
+        res = handler.handle(mockInput);
+        assertTrue(res.isPresent());
+        response = res.get();
+        assertEquals(1, haus.counter);
+        assertEquals(0,haus.fragenWiederholung);
+        assertTrue(response.getOutputSpeech().toString().contains("Dann legen wir los! Hier kommt Frage "));
+
+
+        haus.STATUS_ID = 3;
+        res = handler.handle(mockInput);
+        assertTrue(res.isPresent());
+        response = res.get();
+        assertEquals(3, haus.EINSTELLUNGS_ID);
+        assertTrue(response.getOutputSpeech().toString().contains("Wie viele Spieler seid ihr?"));
+
+        haus.STATUS_ID = 3;
+        res = handler.handle(mockInput);
+        assertTrue(res.isPresent());
+        response = res.get();
+        assertEquals(3, haus.EINSTELLUNGS_ID);
+        assertTrue(response.getOutputSpeech().toString().contains(""));
+
+        haus.STATUS_ID = 4;
+        res = handler.handle(mockInput);
+        assertTrue(res.isPresent());
+        response = res.get();
+        assertTrue(response.getOutputSpeech().toString().contains(""));
+
+        haus.STATUS_ID = 5;
+        res = handler.handle(mockInput);
+        assertTrue(res.isPresent());
+        response = res.get();
+        assertTrue(response.getOutputSpeech().toString().contains(""));
+
+        haus.STATUS_ID = 6;
+        res = handler.handle(mockInput);
+        assertTrue(res.isPresent());
+        response = res.get();
+        assertTrue(response.getOutputSpeech().toString().contains(""));
+
+        haus.STATUS_ID = 7;
+        res = handler.handle(mockInput);
+        assertTrue(res.isPresent());
+        response = res.get();
+        assertTrue(response.getOutputSpeech().toString().contains("Schön das du da warst Ich hoffe wir sehen uns bald wieder"));
+
+        haus.STATUS_ID = 10000;
+        res = handler.handle(mockInput);
+        assertTrue(res.isPresent());
+        response = res.get();
+        assertTrue(response.getOutputSpeech().toString().contains("Diesen Input konnte ich nicht verarbeiten"));
+
+
+
+    }
 }
